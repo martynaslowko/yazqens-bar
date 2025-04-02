83e6e474ee657bd210e85eb022987e260fa4b64f
martynaslowko <martislowko@gmail.com>
2025-04-02 22:33:22 +0200

    init

---
+++ src/main/kotlin/ttv/yazqen/bar/YazqensBarApplication.kt
@@ -0,0 +1,11 @@
+package ttv.yazqen.bar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YazqensBarApplication
+
+fun main(args: Array<String>) {
+    runApplication<YazqensBarApplication>(*args)
+}
